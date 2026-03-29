3d0264506db7c6178d7974797604e5cbd816ff31
Akanksha Duvvuri <akanksha.duvvuri16@gmail.com>
2026-03-29 11:05:48 +0530

    Recursion part 1

--- recursion/power.cpp
+++ recursion/power.cpp
@@ -6,7 +6,25 @@ int power(int n, int expo){
     return n * power(n, expo - 1);
 }
 
+//optimised - TC = O(logn)
+
+int pow(int x, int n) {
+    if(n == 0) {
+        return 1;
+    }
+
+    int halfpow = pow(x, n/2);
+    int halfpowsquare = halfpow * halfpow;
+
+    if(n % 2 != 0){
+        return x * halfpowsquare;
+    }
+
+    return halfpowsquare;
+}
+
 int main(){
     cout << power(2,3) << endl;
+    cout << pow(2,5) << endl;
     return 0;
 }
